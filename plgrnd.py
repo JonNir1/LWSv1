050f90f3f0081c3535e8787de9e076da18d0816d
JonNir <jonathan.nir@mail.huji.ac.il>
2025-05-08 18:04:14 +0300

    update

--- plgrnd.py
+++ plgrnd.py
@@ -11,4 +11,5 @@ SUBJ_PATH = os.path.join(cnfg.RAW_DATA_PATH, f"{SUBJ_PREFIX} GalChen Demo")
 
 subj = Subject.from_raw(exp_name=cnfg.EXPERIMENT_NAME, subject_id=1, session=1, dirname="v4-1-1 GalChen Demo")
 data = subj.get_behavior()
+trial10 = data[data[cnfg.TRIAL_STR] == 10]
 
