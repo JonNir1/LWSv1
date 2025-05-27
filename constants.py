de46f9510a1f8d57b07e10cc6d6b4583575b5601
JonNir <jonathan.nir@mail.huji.ac.il>
2025-05-27 16:09:44 +0300

    nit

--- constants.py
+++ constants.py
@@ -1,18 +1,21 @@
 import numpy as np
 
-
+## GENERAL ##
 MISSING_VALUE = np.nan
 DATE_TIME_FORMAT = "%m-%d-%Y %H:%M:%S"
 
-TIME_STR = "time"
-START_TIME_STR, END_TIME_STR = f"start_{TIME_STR}", f"end_{TIME_STR}"
-
+## STRINGS ##
+SUBJECT_STR, SESSION_STR = "subject", "session"
+BLOCK_STR, TRIAL_STR = "block", "trial"
 TRIGGER_STR = "trigger"
 ACTION_STR = "action"
 LABEL_STR = "label"
 EVENT_STR = "event"
 TARGET_STR = "target"
+CONDITION_STR = "condition"
 
+TIME_STR = "time"
+START_TIME_STR, END_TIME_STR = f"start_{TIME_STR}", f"end_{TIME_STR}"
 X, Y = "x", "y"
 PUPIL_STR = "pupil"
 LEFT_STR, RIGHT_STR = "left", "right"
@@ -20,13 +23,11 @@ LEFT_X_STR, LEFT_Y_STR, LEFT_PUPIL_STR = f"{LEFT_STR}_{X}", f"{LEFT_STR}_{Y}", f
 RIGHT_X_STR, RIGHT_Y_STR, RIGHT_PUPIL_STR = f"{RIGHT_STR}_{X}", f"{RIGHT_STR}_{Y}", f"{RIGHT_STR}_{PUPIL_STR}"
 LEFT_LABEL_STR, RIGHT_LABEL_STR = f"{LEFT_STR}_{LABEL_STR}", f"{RIGHT_STR}_{LABEL_STR}"
 LEFT_EVENT_STR, RIGHT_EVENT_STR = f"{LEFT_STR}_{EVENT_STR}", f"{RIGHT_STR}_{EVENT_STR}"
-FIXATION_STR = "fixation"
 
-SUBJECT_STR, SESSION_STR = "subject", "session"
-BLOCK_STR, TRIAL_STR = "block", "trial"
-CONDITION_STR = "condition"
+ALL_STR = "all"
 CATEGORY_STR = "category"
 IMAGE_STR = "image"
 IS_RECORDING_STR = "is_recording"
 DISTANCE_STR = "distance"
 METADATA_STR = "metadata"
+FIXATION_STR = "fixation"
