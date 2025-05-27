a3cc4490b71e7ef7ecf2b8d359ceea61d5077009
JonNir <jonathan.nir@mail.huji.ac.il>
2025-05-28 00:02:03 +0300

    update data explor

--- plgrnd.py
+++ plgrnd.py
@@ -50,28 +50,24 @@ fig.show()
 
 ### LWS IDENTIFICATION ###
 
-# extract specific trial
-trial = subj.get_trials()[9]
-
 # check if fixations don't end too close to the trial end
 MIN_TIME_FROM_TRIAL_END = 100   # ms
-is_trial_end = fixs_df[cnfg.END_TIME_STR] >= trial.end_time - MIN_TIME_FROM_TRIAL_END
+is_trial_end = fixs_df["to_trial_end"] <= MIN_TIME_FROM_TRIAL_END
 del MIN_TIME_FROM_TRIAL_END
 
 
 # check if **current or 1-next** fixations end before target marked
-target_identification_data = trial.get_target_identification_data()  # targets' identification time
 is_before_marking = pd.DataFrame(
-    fixs_df["end_time"].values < target_identification_data["time"].values[:, np.newaxis],
-    columns=fixs_df.index, index=target_identification_data.index,
+    fixs_df["end_time"].values < targets_df["time"].values[:, np.newaxis],
+    columns=fixs_df.index, index=targets_df.index,
 ).T
 is_next_before_marking = pd.concat([
     is_before_marking.xs(DominantEyeEnum.Left, level="eye").shift(-1),
     is_before_marking.xs(DominantEyeEnum.Right, level="eye").shift(-1)
-], keys=[cnfg.LEFT_STR, cnfg.RIGHT_STR], names=is_before_marking.index.names, axis=0)
+], keys=[cnfg.LEFT_STR, cnfg.RIGHT_STR], axis=0)
+is_next_before_marking.index = is_next_before_marking.index.reorder_levels([1, 0, 2]).rename(is_before_marking.index.names)
 # fill nans due to shifting:
 is_next_before_marking = is_next_before_marking.mask(is_next_before_marking.isna(), is_before_marking)
-del target_identification_data
 
 
 # check if the **current or 1-next** fixation is in the bottom strip
@@ -79,7 +75,8 @@ is_in_strip = fixs_df['in_strip']
 is_next_in_strip = pd.concat([
     is_in_strip.xs(DominantEyeEnum.Left, level="eye").shift(-1),
     is_in_strip.xs(DominantEyeEnum.Right, level="eye").shift(-1)
-], keys=[cnfg.LEFT_STR, cnfg.RIGHT_STR], names=is_in_strip.index.names, axis=0)
+], keys=[cnfg.LEFT_STR, cnfg.RIGHT_STR], axis=0)
+is_next_in_strip.index = is_next_in_strip.index.reorder_levels([1, 0, 2]).rename(is_in_strip.index.names)
 # fill nans due to shifting:
 is_next_in_strip = is_next_in_strip.mask(is_next_in_strip.isna(), is_in_strip)
 
@@ -87,12 +84,13 @@ is_next_in_strip = is_next_in_strip.mask(is_next_in_strip.isna(), is_in_strip)
 # number fixations that are close to each target, relative to target-marking (on marking = 0)
 MAX_DEG_FROM_TARGET = 1.0
 px_distances = fixs_df[[col for col in fixs_df.columns if col.startswith(cnfg.TARGET_STR)]]
-is_on_target = px_distances <= (MAX_DEG_FROM_TARGET / trial.px2deg)
+is_on_target = px_distances <= (MAX_DEG_FROM_TARGET / subj.px2deg)
 num_on_target = pd.concat([
     is_on_target.xs(DominantEyeEnum.Left, level="eye").cumsum(),
     is_on_target.xs(DominantEyeEnum.Right, level="eye").cumsum()
-], keys=[cnfg.LEFT_STR, cnfg.RIGHT_STR], names=is_on_target.index.names, axis=0).astype(float)
-num_on_target = num_on_target.mask(~is_on_target, np.nan)
+], keys=[cnfg.LEFT_STR, cnfg.RIGHT_STR], axis=0).astype(float)
+num_on_target.index = num_on_target.index.reorder_levels([1, 0, 2]).rename(is_on_target.index.names)
+num_on_target = num_on_target.where(is_on_target)
 
 marking_fix_num = num_on_target.loc[fixs_df['curr_marked'].notnull()]   # num-on-target for the fixation where target was marked
 marking_fix_num = marking_fix_num.stack().droplevel('fixation').unstack('eye').T
