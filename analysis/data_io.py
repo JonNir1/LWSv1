24a0c82d38cea16f8f7483e883b3e76a4c2705e3
JonNir <jonathan.nir@mail.huji.ac.il>
2025-06-09 17:37:15 +0300

    append fixation data to identification data

--- analysis/data_io.py
+++ analysis/data_io.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 
 import config as cnfg
 from data_models.Subject import Subject
+from data_models.LWSEnums import DominantEyeEnum
 
 _DESCRIPTOR_MAP = {
     cnfg.ACTION_STR: lambda trl: trl.get_actions(),
@@ -13,6 +14,7 @@ _DESCRIPTOR_MAP = {
     cnfg.FIXATION_STR: lambda trl: trl.process_fixations(),
     cnfg.METADATA_STR: lambda trl: trl.get_metadata().to_frame().T.drop(columns=[f"{cnfg.TRIAL_STR}_num"]),
 }
+_MAX_IDENTIFICATION_FIXATION_TIME_DIFF = 200    # ms; max allowed time-diff between target identification and fixation end time
 
 
 def process_trials(
@@ -81,12 +83,18 @@ def identification_data(subject: Subject, save: bool = True, verbose: bool = Fal
     - trial_type: int - type of the trial (see LWSEnums.SearchArrayTypeEnum)
     - is_bad: bool - whether the trial is "bad", i.e., the subject performed "bad" actions during the trial (e.g. mark-and-reject)
     - identified: bool - whether the target was identified in the trial (True if `time` is finite, False otherwise)
+    - left_fixation, right_fixation: the fixation's ID (fixation number in the trial).
+    - left_fixation_start_time, right_fixation_start_time: the start time of the fixation.
+    - left_fixation_end_time, right_fixation_end_time: the end time of the fixation.
+    - left_fixation_target_distance, right_fixation_target_distance: the fixation's distance to the target (in pixels).
     """
-    # TODO: add fixation start-time and time from trial start, for the identification fixation
     path = os.path.join(subject.out_dir, f'{cnfg.IDENTIFIED_STR}_df.pkl')
     try:
         ident_data = pd.read_pickle(path)
     except FileNotFoundError:
+        if verbose:
+            print(f"No identification DataFrame found. Extracting...")
+        # extract identification data from targets and metadata DataFrames
         targets_df = _read_or_extract(subject, cnfg.TARGET_STR, save=False, verbose=False)
         metadata_df = _read_or_extract(subject, cnfg.METADATA_STR, save=False, verbose=False)
         ident_data = pd.merge(
@@ -96,30 +104,54 @@ def identification_data(subject: Subject, save: bool = True, verbose: bool = Fal
         ).reset_index(drop=False)
         ident_data[cnfg.IDENTIFIED_STR] = np.isfinite(ident_data[cnfg.TIME_STR].values)
         ident_data.loc[~ident_data[cnfg.IDENTIFIED_STR].values, cnfg.TIME_STR] = np.nan    # set non-identified times to NaN
+
+        # add fixation-related columns
+        for eye in DominantEyeEnum:
+            for col in ["", f"{cnfg.START_TIME_STR}", f"{cnfg.END_TIME_STR}", f"{cnfg.TARGET_STR}_{cnfg.DISTANCE_STR}"]:
+                col_name = f"{eye}_{cnfg.FIXATION_STR}_{col}".strip("_")
+                ident_data[col_name] = np.nan
+
+        # match fixations to identifications
+        fixs_df = _read_or_extract(subject, cnfg.FIXATION_STR, save=save, verbose=verbose)
+        for i, row in ident_data.iterrows():
+            trial, target, ident_time = row[cnfg.TRIAL_STR], row[cnfg.TARGET_STR], row[cnfg.TIME_STR]
+            if not np.isfinite(ident_time):
+                # no identification time, skip
+                continue
+            for eye in DominantEyeEnum:
+                try:
+                    trial_fixs = fixs_df.xs((trial, eye), level=[cnfg.TRIAL_STR, "eye"])
+                except KeyError as _e:
+                    # no fixations in this trial for this eye, skip
+                    continue
+                # check if identified during a fixation
+                during = trial_fixs[
+                    (trial_fixs[cnfg.START_TIME_STR] <= ident_time) & (ident_time <= trial_fixs[cnfg.END_TIME_STR])
+                ]
+                if not during.empty:
+                    # identified during a fixation, take the only one
+                    assert len(during) == 1
+                    chosen = during.iloc[0]
+                else:
+                    # not identified during a fixation, find the closest preceding fixation
+                    delta_t = ident_time - trial_fixs[cnfg.START_TIME_STR]
+                    trial_fixs_before = trial_fixs[
+                        # only consider fixations that ended before identification and within the allowed time-difference
+                        (0 <= delta_t) & (delta_t <= _MAX_IDENTIFICATION_FIXATION_TIME_DIFF)
+                    ].copy()
+                    if trial_fixs_before.empty:
+                        # no fixations before identification, skip
+                        continue
+                    trial_fixs_before["delta_t"] = ident_time - trial_fixs_before[cnfg.END_TIME_STR]
+                    chosen = trial_fixs_before.loc[trial_fixs_before["delta_t"].idxmin()]
+                # write chosen fixation's data to identifications DataFrame
+                ident_data.at[i, f"{eye}_{cnfg.FIXATION_STR}"] = chosen.name  # fixation ID
+                ident_data.at[i, f"{eye}_{cnfg.FIXATION_STR}_{cnfg.START_TIME_STR}"] = chosen[cnfg.START_TIME_STR]
+                ident_data.at[i, f"{eye}_{cnfg.FIXATION_STR}_{cnfg.END_TIME_STR}"] = chosen[cnfg.END_TIME_STR]
+                ident_data.at[i, f"{eye}_{cnfg.FIXATION_STR}_{cnfg.TARGET_STR}_{cnfg.DISTANCE_STR}"] = chosen[target]
     if save:
         ident_data.to_pickle(path)
     return ident_data
-
-
-def fixation_data(subject: Subject, save: bool = True, verbose: bool = False) -> pd.DataFrame:
-    """
-    Extracts the fixation DataFrame for a given subject and saves it as a pickle file.
-    If the DataFrame already exists, it is read from the pickle file. If not, it is generated from the subject's
-    trials and saved to the subject's output directory if `save` is True.
-
-    The DataFrame is indexed by (trial number, eye, fixation ID), with columns:
-    - start-time, end-time, duration: float (in ms)
-    - center-pixel: tuple (x, y) - the mean pixel coordinates of the fixation
-    - pixel_std: tuple (x, y) - the standard deviation of the pixel coordinates of the fixation
-    - outlier_reasons: List[str] - reasons for the fixation being an outlier (or [] if not an outlier)
-    - target_0, target_1, ...: float - pixel-distances to each target in the trial
-    - all_marked: List[str] - all targets that were identified previously or during the current fixation
-    - curr_marked: str - the target that was identified during the current fixation (or None)
-    - in_strip: bool - whether the fixation is in the bottom strip of the trial
-    - from_trial_start: float - time from trial's start to the start of the fixation (in ms)
-    - to_trial_end: float - time from fixation's end to the end of the trial (in ms)
-    """
-    return _read_or_extract(subject, cnfg.FIXATION_STR, save=save, verbose=verbose)
 
 
 def _read_or_extract(subject: Subject, descriptor: str, save: bool = True, verbose: bool = False) -> pd.DataFrame:
