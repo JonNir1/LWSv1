950d9d2e281cea15a74fb83a351866bc14891c8d
JonNir <jonathan.nir@mail.huji.ac.il>
2025-07-06 21:47:57 +0300

    create LWS funnel

---
+++ analysis/figures/funnel_fig.py
@@ -0,0 +1,47 @@
+from typing import Literal
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+
+import config as cnfg
+from pre_process.lws_funnel import LWS_FUNNEL_STEPS
+
+
+def create_funnel_figure(data: pd.DataFrame, funnel_type: Literal["fixations", "visits"]) -> go.Figure:
+    """ Creates a funnel figure for the provided multi-subject fixations/visits data. """
+    funnel_sizes = _calc_funnel_sizes(data)
+    fig = go.Figure()
+    for subj_id, subj_funnel in funnel_sizes.groupby(cnfg.SUBJECT_STR):
+        fig.add_trace(
+            go.Funnel(
+                name=f"Subject {subj_id}", legendgroup=f"Subject {subj_id}",
+                y=subj_funnel["step"], x=subj_funnel["size"],
+                textinfo="label+value+percent initial",
+                marker=dict(color=px.colors.qualitative.Pastel[int(subj_id)]),
+                connector=dict(visible=False),
+            )
+        )
+    fig.update_layout(
+        width=800, height=600,
+        title=dict(text=f"LWS-{funnel_type.capitalize()} Funnel", font=cnfg.TITLE_FONT),
+    )
+    return fig
+
+
+def _calc_funnel_sizes(data: pd.DataFrame,) -> pd.DataFrame:
+    funnel_steps = [step for step in LWS_FUNNEL_STEPS if step in data.columns]
+    funnel_sizes = dict()
+    for subj_id, subj_fixations in data.groupby(cnfg.SUBJECT_STR):
+        for i in range(len(funnel_steps)):
+            curr_step = funnel_steps[i]
+            curr_and_prev_steps = funnel_steps[:i + 1]
+            step_size = subj_fixations[curr_and_prev_steps].all(axis=1).sum()
+            funnel_sizes[(subj_id, curr_step)] = step_size
+    funnel_sizes = (
+        pd.Series(funnel_sizes)
+        .reset_index(drop=False)
+        .rename(columns={"level_0": cnfg.SUBJECT_STR, "level_1": "step", 0: "size"})
+    )
+    return funnel_sizes
--- plgrnd2.py
+++ plgrnd2.py
@@ -1,12 +1,13 @@
-from typing import Dict
+from typing import Literal
 
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 import plotly.io as pio
 
 import config as cnfg
-from pre_process.pipeline import parse_subject, process_subject
+from pre_process.pipeline import read_subject, process_subject
 
 pio.renderers.default = "browser"
 
@@ -16,8 +17,8 @@ pio.renderers.default = "browser"
 _SUBJECTS = [(1, "v4-1-1 GalChen Demo"), (2, "v4-2-1 Netta Demo"), (3, "v4-3-1 Rotem Demo")]
 PROCESSED_SUBJECTS = dict()
 for i, (subject_id, data_dir) in enumerate(_SUBJECTS):
-    subj = parse_subject(
-        subject_id=subject_id, exp_name=cnfg.EXPERIMENT_NAME, data_dir=data_dir, session=1, verbose=True
+    subj = read_subject(
+        subject_id=subject_id, exp_name=cnfg.EXPERIMENT_NAME, session=1, data_dir=data_dir, verbose=True
     )
     targets, metadata, idents, fixations, visits = process_subject(subj, verbose=True)
     results = {
@@ -31,31 +32,19 @@ for i, (subject_id, data_dir) in enumerate(_SUBJECTS):
     PROCESSED_SUBJECTS[subject_id] = results
 del _SUBJECTS, i, subject_id, data_dir, subj, targets, metadata, idents, fixations, visits, results
 
+def concat_subject_results(to_concat: Literal["identification", "fixation", "visit"]) -> pd.DataFrame:
+    conatenated = (
+        pd.concat([subj_res[to_concat] for subj_res in PROCESSED_SUBJECTS.values()], keys=PROCESSED_SUBJECTS.keys(), axis=0)
+        .reset_index(drop=False)
+        .rename(columns={"level_0": cnfg.SUBJECT_STR})
+        .drop(columns=["level_1"])
+    )
+    return conatenated
+all_idents = concat_subject_results("identification")
+all_fixations = concat_subject_results(cnfg.FIXATION_STR)
+all_visits = concat_subject_results(cnfg.VISIT_STR)
+
 
 ## LWS Funnel - fixations
-from analysis.lws_funnel import fixation_funnel
-fixs = get_fixations(SUBJECT[0], save=True, verbose=True)
-fixs_funnel = fixation_funnel(fixations=fixs)
-fix_funnel_sizes = {k: len(v) for (k, v) in fixs_funnel.items()}
-fix_funnel_fig = go.Figure(go.Funnelarea(
-    labels=list(fix_funnel_sizes.keys()),
-    values=list(fix_funnel_sizes.values()),
-    textinfo="label+value",
-    title=dict(text="<b>LWS Funnel - Fixations</b>", font=dict(size=20)),
-))
-fix_funnel_fig.show()
-lws_fixations = fixs_funnel["not_end_with_trial"]
-
-## LWS Funnel - visits
-from analysis.lws_funnel import visit_funnel
-visits = get_visits(SUBJECT[0], save=True, verbose=True)
-visits_funnel = visit_funnel(visits=visits, distance_col="weighted_distance")
-vis_funnel_sizes = {k: len(v) for (k, v) in visits_funnel.items()}
-vis_funnel_fig = go.Figure(go.Funnelarea(
-    labels=list(vis_funnel_sizes.keys()),
-    values=list(vis_funnel_sizes.values()),
-    textinfo="label+value",
-    title=dict(text="<b>LWS Funnel - Visits</b>", font=dict(size=20)),
-))
-vis_funnel_fig.show()
-lws_visits = visits_funnel["not_end_with_trial"]
+from analysis.figures.funnel_fig import create_funnel_figure
+create_funnel_figure(all_fixations, "fixations").show()
