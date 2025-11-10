3aa6591256a683ece516ba2d0d1e9e8a496f470f
jonnir <nir.jonathan@gmail.com>
2025-11-10 13:59:15 -0400

    upadte `on-target` threshold based on distance from target when action took place

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ BAD_ACTIONS = [
     act != SubjectActionCategoryEnum.NO_ACTION and act not in IDENTIFICATION_ACTIONS
 ]
 
-ON_TARGET_THRESHOLD_DVA = 1.0           # threshold to determine if a gaze/fixation is on-target
+ON_TARGET_THRESHOLD_DVA = 1.75          # threshold to determine if a gaze/fixation is on-target
 MAX_GAZE_TO_TRIGGER_TIME_DIFF = 5       # Maximum allowed time difference between gaze and trigger events for them to be considered as part of the same event.
 VISIT_MERGING_TIME_THRESHOLD = 1000     # temporal window for considering two events as part of the same visit, in milliseconds
 
